a66ab9320e7cbab72e26b9612d8d9c4a695b6471
unknown <himanshukr115@gmail.com>
2024-06-21 23:51:25 +0530

    updating UPI details

--- src/pages/PaymentPage.jsx
+++ src/pages/PaymentPage.jsx
@@ -15,14 +15,15 @@ function PaymentPage() {
       setAmtToBePaid(variant.price);
     }
   }, []); // Empty dependency array means this runs once after initial render
-
-  const upiId = "himanshukr1151@axl";
-  const payeeName = "S K Enterprises";
-  const transactionNote = "Payment for S K Enterprises";
+  
+  const upiId = "9099681841@hdfcbank";
+  const payeeName = "Ganesh Pharma";
+  const transactionNote = "Payment for Payee";
 
   const initiateUPIPayment = () => {
-    const upiLink = `upi://pay?pa=${upiId}&pn=${payeeName}&am=${amtToBePaid}&tn=${transactionNote}`;
-
+    console.log("i am called")
+    const upiLink = `upi://pay?pa=${encodeURIComponent(upiId)}&pn=${encodeURIComponent(payeeName)}&am=${encodeURIComponent(amtToBePaid)}&tn=${encodeURIComponent(transactionNote)}&cu=INR`;
+    console.log(`upi link ${upiLink}`)
     // Open the UPI link, which will prompt the user to choose an app
     window.location.href = upiLink;
   };
